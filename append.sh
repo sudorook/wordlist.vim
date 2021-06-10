d560808b0cd168218428f8899efba11a9fc06844
sudorook <daemon@nullcodon.com>
2021-06-10 09:59:53 -0500

    use temp files for sorting

--- append.sh
+++ append.sh
@@ -2,12 +2,15 @@
 set -eu
 
 VIMSPELLDIR=${HOME}/.vim/spell/
+TMP="$(mktemp)"
+
+trap 'rm -f "${TMP}"; exit' INT TERM ERR EXIT
 
 mkdir -p "${VIMSPELLDIR}"
 if [ -f "${VIMSPELLDIR}/en.utf-8.add" ]; then
-  cat spell/en.utf-8.add >> "${VIMSPELLDIR}/en.utf-8.add"
-  sort -u "${VIMSPELLDIR}/en.utf-8.add" > /tmp/temp-$$.add
-  mv /tmp/temp-$$.add "${VIMSPELLDIR}/en.utf-8.add"
+  cat "${VIMSPELLDIR}/en.utf-8.add" > "${TMP}"
+  cat "spell/en.utf-8.add" >> "${TMP}"
+  sort -u "${TMP}" > "${VIMSPELLDIR}/en.utf-8.add"
 else
   cp spell/en.utf-8.add "${VIMSPELLDIR}/en.utf-8.add"
 fi
--- update.sh
+++ update.sh
@@ -2,10 +2,13 @@
 set -eu
 
 VIMSPELLDIR=${HOME}/.vim/spell/
+TMP="$(mktemp)"
+
+trap 'rm -f "${TMP}"; exit' INT TERM ERR EXIT
 
 mkdir -p "${VIMSPELLDIR}"
 if [ -f "${VIMSPELLDIR}/en.utf-8.add" ]; then
-  cat "${VIMSPELLDIR}/en.utf-8.add" >> spell/en.utf-8.add
-  sort -u spell/en.utf-8.add > /tmp/temp-$$.add
-  mv /tmp/temp-$$.add spell/en.utf-8.add
+  cat "${VIMSPELLDIR}/en.utf-8.add" > "${TMP}"
+  cat "spell/en.utf-8.add" >> "${TMP}"
+  sort -u "${TMP}" > spell/en.utf-8.add
 fi
